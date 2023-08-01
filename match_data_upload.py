9bb06dc44473c64f3b2e5eb4b3dc3b84e487cf52
bsczekan <bsczekan@gmail.com>
2023-08-01 15:11:21 -0400

    match data entry document linking v0.1 done
    checks the DB for each player, team, and match to see if they exist. if not they will be created, otherwise the existing document will be used
    
    checks to make sure each player is assigned to each team's roster, and that each player has the team's id in its teams_id list
    
    checks that each team has the match in its matches list, and that the match properly stores the two teams' ids
    
    checks that each player has the match id listed in its matches list

--- match_data_upload.py
+++ match_data_upload.py
@@ -6,9 +6,10 @@ from models.player import Player
 
 insert_one_res = pymongo.results.InsertOneResult
 
+
 # from models.competition import Competition
 # from models.player import Player
-# TODO: fix matches from uploading multiple times, checking for the document isn't working
+# TODO: make code more dry
 
 
 def split_match_data(match_data):
@@ -35,12 +36,17 @@ def split_match_data(match_data):
     check_team_match_relation(team_one_doc, match_doc)
     check_team_match_relation(team_two_doc, match_doc)
 
+    check_player_match_relation(team_one_player_docs, match_doc)
+    check_player_match_relation(team_two_player_docs, match_doc)
+
 
 def check_team_match_relation(team_doc, match_doc):
     team_id = get_doc_id(team_doc)
     match_id = get_doc_id(match_doc)
-    if not team_id in app.db.matches.find_one(match_id):
+    if team_id not in app.db.matches.find_one(match_id):
         app.db.matches.update_one({'_id': match_id}, {'$addToSet': {'teams_id': team_id}})
+    if match_id not in app.db.teams.find_one(team_id):
+        app.db.teams.update_one({'_id': team_id}, {'$addToSet': {'matches': match_id}})
 
 
 def get_doc_id(doc):
@@ -50,9 +56,21 @@ def get_doc_id(doc):
 def check_player_team_relation(player_docs, team_doc):
     team_id = get_doc_id(team_doc)
     for player in player_docs:
-        player_id = player.inserted_id if type(player) is insert_one_res else player['_id']
-        if not team_id in app.db.players.find_one(player_id)['teams_id']:
+        # player_id = player.inserted_id if type(player) is insert_one_res else player['_id']
+        player_id = get_doc_id(player)
+        if team_id not in app.db.players.find_one(player_id)['teams_id']:
             app.db.players.update_one({'_id': player_id}, {'$addToSet': {'teams_id': team_id}})
+        if player_id not in app.db.teams.find_one(team_id):
+            app.db.teams.update_one({'_id': team_id}, {'$addToSet': {'roster': player_id}})
+
+
+def check_player_match_relation(player_docs, match_doc):
+    match_id = get_doc_id(match_doc)
+    for player in player_docs:
+        # player_id = player.inserted_id if type(player) is insert_one_res else player['_id']
+        player_id = get_doc_id(player)
+        if match_id not in app.db.players.find_one(player_id):
+            app.db.players.update_one({'_id': player_id}, {'$addToSet': {'matches': match_id}})
 
 
 def create_player_docs(players_data):
@@ -86,12 +104,12 @@ def create_team_doc(team_data):
 def create_match_doc(comp_data):
     data_keys = list(comp_data.keys())
     name = comp_data[data_keys[0]]
-    year = comp_data[data_keys[1]]
-    round = comp_data[data_keys[2]]
-    fixture = comp_data[data_keys[3]]
-    db_match = list(app.db.matches.find({'name': name, 'year': year, 'round': round, 'fixture': fixture}))
-    if len(db_match) == 0:
+    year = int(comp_data[data_keys[1]])
+    round = int(comp_data[data_keys[2]])
+    fixture = int(comp_data[data_keys[3]])
+    db_matches = list(app.db.matches.find({'competition': name, 'year': year, 'round': round, 'fixture': fixture}))
+    if len(db_matches) == 0:
         match = Match(name, year, round, fixture)
         return app.db.matches.insert_one(match.to_mongo())
-    else:
-        return db_match
+    elif len(db_matches) >= 1:
+        return db_matches[0]
