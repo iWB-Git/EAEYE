39789ad950f17f751b03e9575622d533345c8035
bsczekan <bsczekan@gmail.com>
2023-08-08 14:29:35 -0400

    added more endpoints for database access based on various parameters
    created helper method to append data to html response
    
    getting 403 errors when trying to access api off local machine. please work on heroku

--- models/competition.py
+++ models/competition.py
@@ -3,3 +3,9 @@ from mongoengine import *
 
 class Competition(Document):
     name = StringField(required=True)
+    teams = ListField(ReferenceField('Team', dbref=True, default=[]))
+
+    def __init__(self, *args, **values):
+        super().__init__(*args, **values)
+        self.name = values['name']
+        self.teams = values['teams']
--- models/team.py
+++ models/team.py
@@ -5,9 +5,11 @@ class Team(Document):
     name = StringField(required=True)
     roster = ListField(ReferenceField('Player', dbref=True, default=[]))
     matches = ListField(ReferenceField('Match', dbref=True, default=[]))
+    comps = ListField(ReferenceField('Competition', dbref=True, default=[]))
 
     def __init__(self, *args, **values):
         super().__init__(*args, **values)
         self.name = values['name']
         self.roster = values['roster']
         self.matches = values['matches']
+        self.comps = values['comps']
