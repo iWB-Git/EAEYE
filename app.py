80176f0172e027af8043c5014113d842c09af2c2
bsczekan <bsczekan@gmail.com>
2023-08-11 13:09:40 -0400

    implementing insert player endpoint

--- app.py
+++ app.py
@@ -179,13 +179,13 @@ def insert_player():
         team_id = player_data['team_id']
         new_player = Player(name=name, dob=dob, nationality=nationality, jersey_num=jersey_num)
         player_club = PlayerTeam(team_id=ObjectId(team_id), reg_date=None, on_team=True)
-        new_player['teams'].append(player_club)
+        new_player['teams'].append(player_club.to_mongo())
         new_player['supporting_file'] = supporting_file
-        db.players.insert_one(new_player.to_mongo)
+        db.players.insert_one(new_player.to_mongo())
         return SUCCESS_201
     except Exception as e:
         traceback.print_exception(type(e), e, e.__traceback__)
-        return ERROR_400
+        return edit_html_desc(ERROR_400, str(e))
 
 
 @app.route('/api/v1/update-one/<collection>', methods=['POST'])
